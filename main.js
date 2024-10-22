00d098df8264e7afe149486b370b96dbb3643d7c
rememberber <rememberber@163.com>
2024-10-22 21:53:58 +0800

    集成Electron

---
+++ main.js
@@ -0,0 +1,30 @@
+import { app, BrowserWindow } from 'electron';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: join(__dirname, 'preload.js'),
+    },
+  });
+
+  mainWindow.loadFile('dist/index.html');
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
